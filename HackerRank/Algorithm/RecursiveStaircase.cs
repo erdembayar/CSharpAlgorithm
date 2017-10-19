2adfce079945eb0db5d32fb715405885d1a94d0d
gitter007 <erkashine@yahoo.com>
2017-10-19 17:12:56 -0500

    Testing and benchmarking. Dynamic programming best.

--- HackerRank/Algorithm/RecursiveStaircase.cs
+++ HackerRank/Algorithm/RecursiveStaircase.cs
@@ -1,14 +1,100 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//https://www.hackerrank.com/challenges/ctci-recursive-staircase
 //https://www.youtube.com/watch?v=eREiwuvzaUM
 
 namespace HackerRank.Algorithm
 {
     class RecursiveStaircase
     {
+        static void Main(string[] args)
+        {
+            int steps = 32;
+            
+
+            var sw = Stopwatch.StartNew();
+            int result = countPathsRecursive(steps);
+            sw.Stop();
+
+            Console.WriteLine("Brute force recursive:" + result);
+            Console.WriteLine("Time elapse:"+sw.Elapsed);
+
+            sw.Reset();
+            sw.Start();
+            int[] memo = new int[steps + 1];
+
+            result = countPathsMemozation(memo, steps);
+            sw.Stop();
+
+            Console.WriteLine("Memoziation recursive:" + result);
+            Console.WriteLine("Time elapse:" + sw.Elapsed);
+
+            sw.Reset();
+            sw.Start();
+
+            result = countPathDynamicProgramming(steps);
+            sw.Stop();
+
+            Console.WriteLine("Dynamic programming:" + result);
+            Console.WriteLine("Time elapse:" + sw.Elapsed);
+
+            Console.ReadKey();
+
+            
+        }
+
+        static int countPathsRecursive(int steps)
+        {
+            if (steps < 0)
+                return 0;
+            if (steps == 0 || steps == 1)
+                return 1;
+
+            return countPathsRecursive(steps - 1) + countPathsRecursive(steps - 2) + countPathsRecursive(steps-3);
+        }
+
+        static int countPathsMemozation(int[] memo, int steps)
+        {
+            if (steps < 0)
+                return 0;
+            if (steps == 0 || steps == 1)
+                return 1;
+
+            if (steps == 2)
+                return 2;
+
+            if (memo[steps]==0)
+                return countPathsMemozation(memo, steps - 1) + countPathsMemozation(memo, steps - 2) + countPathsMemozation(memo, steps - 3);
+
+            return memo[steps];
+        }
+
+        static int countPathDynamicProgramming(int steps)
+        {
+            if (steps < 0)
+                return 0;
+            if (steps == 0 || steps == 1)
+                return 1;
+
+            int[] paths = new int[steps + 1];
+            paths[0] = 1;
+            paths[1] = 1;
+            paths[2] = 2;
+
+            for (int i = 3; i <= steps; i++)
+            {
+                paths[steps] = paths[steps - 1] + paths[steps - 2] + paths[steps - 3];
+            }
+
+            return paths[steps];
+        }
+
     }
+
+
 }
--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -7,33 +7,33 @@ using HackerRank.Sorting;
 
 namespace HackerRank
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            //int n = Convert.ToInt32(Console.ReadLine());
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        //int n = Convert.ToInt32(Console.ReadLine());
 
-            int[] a = new int[] { 5, 7, 1, 10, 16, 20, 3 };
+    //        int[] a = new int[] { 5, 7, 1, 10, 16, 20, 3 };
 
-            int result = BubbleSortImp.BubbleSort(a);
+    //        int result = BubbleSortImp.BubbleSort(a);
 
-            Console.WriteLine(result);
+    //        Console.WriteLine(result);
 
-            Console.WriteLine("Bubble sort:"+ string.Join(",",a));
+    //        Console.WriteLine("Bubble sort:"+ string.Join(",",a));
 
-            a = new int[] { 5, 7, 1, 10, 16, 20, 3 };
+    //        a = new int[] { 5, 7, 1, 10, 16, 20, 3 };
 
-            SelectionSorting.SelectionSort(a);
+    //        SelectionSorting.SelectionSort(a);
 
-            Console.WriteLine("Selection sort:"+string.Join(",", a));
+    //        Console.WriteLine("Selection sort:"+string.Join(",", a));
 
-            a = new int[] { 5, 7, 1, 10, 16, 20, 3 };
+    //        a = new int[] { 5, 7, 1, 10, 16, 20, 3 };
 
-            InsertionSorting.InsertionSort(a);
+    //        InsertionSorting.InsertionSort(a);
 
-            Console.WriteLine("Insertion sort:" + string.Join(",", a));
+    //        Console.WriteLine("Insertion sort:" + string.Join(",", a));
 
-            Console.ReadKey();
-        }
-    }
+    //        Console.ReadKey();
+    //    }
+    //}
 }
